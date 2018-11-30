d66d350f9dd3ad2ca7197e614c127916252f3821
Adrien Toubiana <adtoubia@microsoft.com>
2018-11-30 15:03:27 +0100

    Add SplitView with content
    - Should be replaced by a grid or something else that doesn't hide content

--- RssReader.UWP/HelperFunctions.cs
+++ RssReader.UWP/HelperFunctions.cs
@@ -1,12 +1,24 @@
 ﻿namespace RssReader.UWP
 {
+    using System;
     using Windows.UI.Text;
+    using RssReader.Library;
 
     public static class HelperFunctions
     {
         public static FontWeight ConvertReadToFontWeight(bool isRead)
         {
             return isRead ? FontWeights.Normal : FontWeights.Bold;
+        }
+
+        public static Uri FeedItemToUri(object feedItem)
+        {
+            var item = feedItem as FeedItem;
+            if (item == null)
+            {
+                return new Uri("https://www.bing.com/");
+            }
+            return new Uri(item.Link);
         }
     }
 }
